d8431976c51e778adf3c99a2a6a98786f9f605b4
Neal Kaviratna <Nkaviratna@gatech.edu>
2015-03-12 10:39:58 -0400

    Push content for demo
    added HTML page as well as test audio and images for progress demo.

--- 3DSound.js
+++ 3DSound.js
@@ -1,16 +1,18 @@
 // 3D Sound object
 function ThreeDimensionalSound (GPSCoord, soundfile, eventTrigger, loop) {
     this.GPSCoord = GPSCoord;
-    this.trigger = trigger;
+    this.trigger = eventTrigger;
     this.isPLaying = false;
     this.isLooping = loop;
     
     // Add a listener if a trigger is passed in to start the audio. If no trigger is wanted, null should be passed in for eventTrigger
-    this.audio = new Audio(soundfile);
-    if (eventTrigger) audio.addEventListener(eventTrigger, ThreeDimensionalSound.prototype.play);
+    this.audio = new Howl({
+          urls: [soundfile]
+        });
+    if (this.trigger) audio.addEventListener(eventTrigger, ThreeDimensionalSound.prototype.play);
     
     // TODO: take the GPSCoordinate data and determine what x/y/z values to use based on current position
-    // this.audio.pos3d(x,y,z);
+    this.audio.pos3d(GPSCoord[0],GPSCoord[1],GPSCoord[2]);
 }
 
 ThreeDimensionalSound.prototype.play = function() {
@@ -18,18 +20,18 @@ ThreeDimensionalSound.prototype.play = function() {
     
     // Loop the audio if looping is enabled. Using this event listener implementation instead of Howler's .loop as there have been reported issues with the latter
     var self = this;
-    if (this.isLooping) {
-        this.audio.addEventListener('ended', function() {
-            this.currentTime = 0;
-            this.play();
-        }, false);
-    }
-    else {
-        this.audio.addEventListener('ended', function() {
-            this.currentTime = 0;
-            self.isPlaying = false;
-        }, false);
-    }
+    // if (this.isLooping) {
+        // this.audio.on('ended', function() {
+            // this.currentTime = 0;
+            // this.play();
+        // });
+    // }
+    // else {
+        // this.audio.on('ended', function() {
+            // this.currentTime = 0;
+            // self.isPlaying = false;
+        // });
+    // }
     this.audio.play();
 }
 
